21ae5371da92bb6029960c2c7073ee1de4962b33
marxlima <marxmaciel@gmail.com>
2024-03-13 19:39:33 -0300

    creating first component

--- src/views/tasks/TaskView.vue
+++ src/views/tasks/TaskView.vue
@@ -1,3 +1,31 @@
 <template>
     <h1>Lista de Tarefas</h1>
+
+    <ul>
+        <li v-for="todo in todos" :key="todo.identify">
+            {{ todo.name }}
+        </li>
+    </ul>
+    <input type="text" v-model="name">
 </template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    name: 'TaskView',
+    setup() {
+        const todos = [
+            {identify:1, name: 'tarefa 01', completed: true},
+            {identify:2, name: 'tarefa 02', completed: true},
+            {identify:3, name: 'tarefa 03', completed: true},
+
+        ]
+        const name = ref('defalt value') //reativa
+        name.value = 'teste 2' //mudar valor reativo
+        return {
+            todos,
+            name
+        }
+    }
+}
+</script>
